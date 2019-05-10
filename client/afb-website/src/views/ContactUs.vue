492cbbeee88debe832a45c4eb39c81f9ea97f2fd
BasedHuynh <davis.huynh97@gmail.com>
2019-05-10 16:32:40 -0700

    Contact us form responsive

--- client/afb-website/src/views/ContactUs.vue
+++ client/afb-website/src/views/ContactUs.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="contact">
     <form>
-      <div class="form-group row justify-content-center">
-        <label class="col-7 col-form-label" for="text">Name</label>
-        <div class="col-7">
-          <input id="text" name="text" type="text" required="required" class="form-control">
+      <div class="form-group row justify-content-center d-flex">
+        <div class="col-md-6 justify-content-center">
+          <label class="offset-md-5 col-md-7 col-form-label phoneDisplay" for="text">First Name:</label>
+          <div class="offset-md-5 col-md-7 phoneDisplay">
+            <input id="FName" name="text" type="text" required="required" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <label class="col-md-7 col-form-label phoneDisplay" for="text">Last Name:</label>
+          <div class="col-md-7 phoneDisplay">
+            <input id="LName" name="text" type="text" required="required" class="form-control">
+          </div>
         </div>
       </div>
       <div class="form-group row justify-content-center">
-        <label for="text1" class="col-7 col-form-label">Company/Organization</label>
-        <div class="col-7">
+        <label for="text1" class="col-md-7 col-sm-12 col-form-label">Company/Organization</label>
+        <div class="col-md-7 col-sm-12 ">
           <input id="text1" name="text1" type="text" class="form-control">
         </div>
       </div>
       <div class="form-group row justify-content-center">
-        <label for="text2" class="col-7 col-form-label">Phone</label>
-        <div class="col-7">
+        <label for="text2" class="col-md-7 col-sm-12  col-form-label">Phone</label>
+        <div class="col-md-7 col-sm-12 ">
           <input
             id="text2"
             name="text2"
@@ -27,19 +35,19 @@
         </div>
       </div>
       <div class="form-group row justify-content-center">
-        <label for="text3" class="col-7 col-form-label">Email</label>
-        <div class="col-7">
+        <label for="text3" class="col-md-7 col-sm-12  col-form-label">Email</label>
+        <div class="col-md-7 col-sm-12 ">
           <input id="text3" name="text3" type="text" class="form-control" required="required">
         </div>
       </div>
       <div class="form-group row justify-content-center">
-        <label for="textarea" class="col-7 col-form-label">Comments or Questions</label>
-        <div class="col-7">
+        <label for="textarea" class="col-md-7 col-sm-12 col-form-label">Comments or Questions</label>
+        <div class="col-md-7 col-sm-12 ">
           <textarea id="textarea" name="textarea" cols="40" rows="5" class="form-control"></textarea>
         </div>
       </div>
-      <div class="form-group row justify-content-center">
-        <div class="offset-5 col-6">
+      <div class="form-group row">
+        <div class="col-12 d-flex justify-content-center">
           <button name="submit" type="submit" class="btn btn-primary">Submit</button>
         </div>
       </div>
@@ -59,7 +67,19 @@ export default {
 .contact {
   margin-top: 150px;
   margin-bottom: 150px;
-  margin-left: 150px;
-  margin-right: 150px
+  margin-left: 125px;
+  margin-right: 125px;
+}
+
+@media (max-width: 767px) {
+  .contact {
+    margin-left: 50px;
+    margin-right: 50px;
+  }
+
+  .phoneDisplay {
+    padding-left: 0;
+    padding-right: 0;
+  }
 }
 </style>
