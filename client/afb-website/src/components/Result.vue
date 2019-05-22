b3daccbe59c6d460969d3ba4326857d5ff9e3910
Liuyang Fu <zzl19970122@gmail.com>
2019-05-22 04:02:46 -0700

    mobile result almost done

--- client/afb-website/src/components/Result.vue
+++ client/afb-website/src/components/Result.vue
@@ -47,7 +47,9 @@
           color="#8e71c7"
           status="text"
           :width="circleSize"
-        >{{correctCount}} / {{totalQ}}</el-progress>
+        >
+          <span :style="{fontSize: '36px'}">{{correctCount}} / {{totalQ}}</span>
+        </el-progress>
       </dir>
     </div>
     <el-tabs class="tabs" v-model="activeName" :stretch="true">
@@ -157,7 +159,7 @@ export default {
 </script>
 <style scoped>
 .result-container {
-  /* overflow: scroll; */
+  overflow: scroll;
 }
 .top-bar {
   padding-top: 50px;
