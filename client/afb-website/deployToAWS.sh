6374bd151aef13e41710108a8af2e25d1b393d33
Liuyang Fu <zzl19970122@gmail.com>
2019-05-23 05:59:54 -0700

    added https support

--- client/afb-website/deployToAWS.sh
+++ client/afb-website/deployToAWS.sh
@@ -6,7 +6,7 @@ ssh -i ~/.ssh/afbAWSkeypair.pem ec2-user@ec2-3-15-53-39.us-east-2.compute.amazon
 docker rm -f test
 docker pull liuyaf/afb-website
 
-docker run -d --name test -p 80:80 liuyaf/afb-website
+docker run -d --name test -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt:ro liuyaf/afb-website
 
 exit
 EOF
