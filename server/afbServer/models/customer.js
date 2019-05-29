712bacb99346a469fc99ea6a7c1f00a7167141f6
Liuyang Fu <zzl19970122@gmail.com>
2019-05-29 12:17:14 -0700

    added server side code, TODO migrate to agefriendlysea and login

---
+++ server/afbServer/models/customer.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+let sectionResponseSchema = mongoose.Schema({
+    sectionResponse: [String]
+})
+
+let businessInfoSchema = mongoose.Schema({
+    businessType: {
+        type: String,
+        required: true
+    },
+    busniessSize: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true
+    }
+})
+
+let CustomerSchema = mongoose.Schema({
+    submittedAt: {
+        type: Date
+    },
+    businessInfo: businessInfoSchema,
+    responses: [
+        [String]
+    ]
+})
+
+let Customer = module.exports = mongoose.model('Customer', CustomerSchema);
+
+module.exports.addCustomerResult = function (result, callback) {
+    let obj = {}
+    let busInfo = {}
+    busInfo.businessType = result.info[0]
+    busInfo.busniessSize = result.info[1]
+    busInfo.zipcode = result.info[2]
+    obj.submittedAt = Date.now();
+    obj.businessInfo = busInfo;
+    obj.responses = result.result;
+    Customer.create(obj, callback);
+}
+
+module.exports.getEmployerResults = function (callback) {
+    Employer.find(callback)
+}
